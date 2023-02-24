4f4105b19d255219e61e9df5631830036f143c49
Tomislav Periša <tomislav.pingi.perisa@gmail.com>
2023-02-24 16:04:45 +0100

    Edited Rook.cs

--- Chess Framework/PieceTypes/Rook.cs
+++ Chess Framework/PieceTypes/Rook.cs
@@ -40,27 +40,6 @@ namespace Chess_Framework.PieceTypes
                         PossibleMoves.Add(square.Row.ToString() + square.Column.ToString());
                     }
                 }
-                if (side == Side.Black)
-                {
-                    if (square.Piece == null && square.Column == _posCol && square.Row == _posRow + 1)
-                    {
-                        PossibleMoves.Add(square.Row.ToString() + square.Column.ToString());
-                        if (_posRow == 1 && square.Piece == null)
-                        {
-                            foreach (Square squareBehind in board)
-                            {
-                                if (squareBehind.Column == square.Column && squareBehind.Row == square.Row + 1 && squareBehind.Piece == null)
-                                {
-                                    PossibleMoves.Add(squareBehind.Row.ToString() + squareBehind.Column.ToString());
-                                }
-                            }
-                        }
-                    }
-                    if (square.Row == _posRow + 1 && ((square.Column == _posCol - 1) || (square.Column == _posCol + 1)) && square.Piece != null && square.Piece.Side == Side.White)
-                    {
-                        PossibleMoves.Add(square.Row.ToString() + square.Column.ToString());
-                    }
-                }
             }
         }
     }
